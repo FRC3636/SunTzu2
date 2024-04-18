4744a5631b65d98a0e86bcca99ca99a029f54668
NikolaJenkins <naojoy5@gmail.com>
2024-04-17 17:32:13 -0700

    sim flywheel

--- src/main/java/frc/robot/subsystems/flywheels/Flywheel.kt
+++ src/main/java/frc/robot/subsystems/flywheels/Flywheel.kt
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Subsystem
 
 object Flywheel: Subsystem {
-    var io = RealFlywheelIO()
+    var io: FlywheelIO = SimFlywheelIO()
 
     fun shoot(): Command {
         return this.runEnd({
--- src/main/java/frc/robot/subsystems/flywheels/FlywheelIO.kt
+++ src/main/java/frc/robot/subsystems/flywheels/FlywheelIO.kt
@@ -3,13 +3,22 @@ package frc.robot.subsystems.flywheels
 import com.revrobotics.CANSparkBase
 import com.revrobotics.CANSparkFlex
 import com.revrobotics.CANSparkLowLevel
+import edu.wpi.first.math.system.plant.DCMotor
 import edu.wpi.first.math.util.Units
 import edu.wpi.first.units.Distance
 import edu.wpi.first.units.Measure
 import edu.wpi.first.units.Units.*
 import edu.wpi.first.units.Velocity
+import edu.wpi.first.wpilibj.simulation.FlywheelSim
 
-class RealFlywheelIO {
+interface FlywheelIO {
+    var leftSpeed: Double
+    var rightSpeed: Double
+    fun setVelocity(speed: Measure <Velocity<Distance>>)
+    fun getVelocity(): Measure<Velocity<Distance>>
+}
+
+class RealFlywheelIO : FlywheelIO {
     val flywheelMotor = CANSparkFlex(11, CANSparkLowLevel.MotorType.kBrushless).apply {
         pidController.apply {
             p = 1.0
@@ -23,9 +32,32 @@ class RealFlywheelIO {
         }
     }
 
-    fun setVelocity(speed: Measure<Velocity<Distance>>) {
+    override fun setVelocity(speed: Measure<Velocity<Distance>>) {
         val angluarSpeed = RadiansPerSecond.of(speed.baseUnitMagnitude() / FLYWHEEL_RADIUS.`in`(Meters))
         flywheelMotor.pidController.setReference(angluarSpeed.baseUnitMagnitude(), CANSparkBase.ControlType.kVelocity)
+    }
+
+    override fun getVelocity(): Measure<Velocity<Distance>> {
+        return MetersPerSecond.of(flywheelMotor.encoder.velocity * FLYWHEEL_RADIUS.`in`(Meters))
+    }
+
+    companion object Constants {
+        val FLYWHEEL_RADIUS = Inches.of(1.5)
+    }
+}
+
+class SimFlywheelIO: FlywheelIO {
+
+    private val leftFlywheel = FlywheelSim(DCMotor.getNeoVortex(1), 1.0, 0.01)
+    private val rightFlywheel = FlywheelSim(DCMotor.getNeoVortex(1), 1.0, 0.01)
+    override fun setVelocity(speed: Measure<Velocity<Distance>>) {
+        leftFlywheel.
+    }
+
+    override fun getVelocity(): Measure<Velocity<Distance>> {
+        return MetersPerSecond.of(leftFlywheel.angularVelocityRadPerSec * FLYWHEEL_RADIUS.`in`(Meters))
+        return MetersPerSecond.of(rightFlywheel.angularVelocityRadPerSec * FLYWHEEL_RADIUS.`in`(Meters))
+
     }
 
     companion object Constants {
